6b5ffd215c20bd05fcb3b27e4e93c5e6c4481bae
Rahul Vudaru <rvudaru@Rahuls-MacBook-Neo.local>
2026-06-15 00:26:55 -0500

    No longer creating a brand new playlist everytime A is pressed, instead adding songs to the same playlist per instance
    need to add a check to make sure a song isn't added twice

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from pynput import keyboard
 import spotify_func
 
 def on_press(key):
+    global playlist_created, playlist_id
     try:
         if not hasattr(key, 'char'):
             pass
             
         elif key.char == 'a':
             print("a was pressed")
+            if playlist_created == False:
+                playlist_id = spotify_func.create_playlist("test", access_token)
+                playlist_created = True
             track_id = spotify_func.get_curr_track(access_token)
-            playlist_id = spotify_func.create_playlist("test", access_token)
             spotify_func.add_track(track_id, playlist_id, access_token)
+
 
     except:
         print("invalid input (what did you press?): {}".format(key))
@@ -22,6 +26,8 @@ def on_release(key):
         # Stop listener if escape key is pressed
         return False
 
+playlist_created = False
+playlist_id = None
 [auth_code, refresh_token, access_token] = spotify_func.authorize_user()
 # Collect events until released
 with keyboard.Listener(
